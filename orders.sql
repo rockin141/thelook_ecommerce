5312b1db35d409c8a3629b0924365a68415963e8
rockin141 <33355909+rockin141@users.noreply.github.com>
2022-09-08 14:11:28 +0700

    Update orders.sql

--- orders.sql
+++ orders.sql
@@ -1,19 +1,19 @@
-select 
+SELECT
       a.order_id,
       a.user_id,
       a.product_id,
-        c.category,
-        c.brand,
-        c.name,
-        b.num_of_item,
-        a.status,
-          extract (date from a.created_at) as order_date,
-          count (a.delivered_at) as completing_order_count,
-          count (a.shipped_at) as shipping_count,
-          count (a.returned_at) as cancel_order_count,
-              sum(case when a.delivered_at is not null then  b.num_of_item * c.retail_price else null end ) as revenue,
-              count(a.order_id) as order_count,
-              SUM((case when a.delivered_at is not null then  b.num_of_item * c.retail_price else null end)- c.cost) as net
+      b.num_of_item,
+      a.status,
+      EXTRACT (date FROM a.created_at) AS order_date,
+      COUNT (a.status = "Complete" ) AS complete_count,
+      COUNT (a.status = "Cancelled" ) AS cancelled_count,
+      COUNT (a.status = "Shipped" ) AS shipped_count,
+      COUNT (a.status = "Processing" ) AS processing_count,
+      COUNT (a.status = "Returned" ) AS returning_count,
+      COUNT(a.order_id) AS order_count,
+      COUNT(a.user_id) AS user_count,
+      SUM(case when a.delivered_at IS NOT NULL THEN  b.num_of_item * c.retail_price ELSE NULL END ) AS revenue,
+      SUM((case when a.delivered_at IS NOT NULL THEN   b.num_of_item * c.retail_price ELSE NULL END)- c.cost) AS net
              
 
 from 
@@ -22,25 +22,24 @@ from
 `bigquery-public-data.thelook_ecommerce.orders`b,
 `bigquery-public-data.thelook_ecommerce.products`c
 
-where a.order_id = b.order_id
-      and a.user_id = b.user_id
-      and a.product_id = c.id
-        group by a.order_id,
+WHERE 
+                  a.order_id = b.order_id
+      AND         a.user_id = b.user_id
+      AND         a.product_id = c.id
+        
+GROUP BY          a.order_id,
                   a.user_id,
                   a.product_id,
                   a.created_at,
-                  a.delivered_at,
-                  a.shipped_at,
-                  a.returned_at,
-                   c.category,
-                   c.brand,
-                   c.name,
-                   b.num_of_item,
-                   a.status
+                  b.num_of_item,
+                  a.status
 
-                        order by order_date, 
-                                shipping_count,
-                                completing_order_count,
-                                cancel_order_count,
-                                order_count,
-                                revenue;
+ORDER BY 
+                  order_date, 
+                  complete_count,
+                  cancelled_count,
+                  shipped_count,
+                  processing_count,
+                  returning_count,
+                  order_count,
+                  revenue;
